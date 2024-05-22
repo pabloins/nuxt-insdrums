abf7567cf58171029406cd09943f3a7158f90f01
pabloins <53983994+pabloins@users.noreply.github.com>
2024-05-22 14:24:31 -0400

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+<!--         <header>
+            <nav>
+                <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/products">Products</NuxtLink></li>
+                </ul>
+                <a href="/about">normal link -- about page</a>
+            </nav>
+        </header> -->
+        
+        <h2>Home</h2>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
